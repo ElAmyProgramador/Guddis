43aedc63d78abc887a611c4114de70356ba78bb7
Psykokwak <alexeivra@gmail.com>
2025-12-29 01:21:19 -0600

    se añade loop en run y se pinta un cuadrilatero

--- src/main/GamePannel.java
+++ src/main/GamePannel.java
@@ -2,9 +2,11 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
-public class GamePannel extends JPanel {
+public class GamePannel extends JPanel implements Runnable {
     // config de panel
     final int originalTileSize = 16; // 16x16
     final int scale = 3;
@@ -20,5 +22,33 @@ public class GamePannel extends JPanel {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+    
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    
+    @Override
+    public void run() {
+        // loop
+        while (gameThread != null) {
+            // actualiza info
+            update();
+            // dibuja
+            repaint();
+        }
+    }
+    
+    public void update() {
+        //
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }

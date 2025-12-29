eb03d477775dd42a74b756aff83a6ceb26819a6d
Psykokwak <alexeivra@gmail.com>
2025-12-29 00:49:58 -0600

    se empieza el desarrollo del juego

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Guddis");
+        
+        GamePannel gamePannel = new GamePannel();
+        window.add(gamePannel);
+        window.pack();
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
